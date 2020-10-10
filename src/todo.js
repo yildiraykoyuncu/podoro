58eb6a76153047d0f6fc02e16e78f60fecc324ed
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-10-10 10:56:45 +0200

    added functionalities to todo

--- src/todo.js
+++ src/todo.js
@@ -10,15 +10,28 @@ export class Todo {
         this.isCompleted = false;
     }
 
+    static addTodoScreenCall(event) {
+
+        //call add todo screen
+        Todo.renderAddTodoWindow();
+    }
+
     static addTodoHandler(event) {
-        if (event.key !== 'Enter') return;
-        const text = event.target.value
-        const todo = new Todo(text);
-        state.todos.push(todo)
 
-        console.log(state.todos)
+        if (event.target.id === 'saveBtn') {
+            const text = document.getElementById('inputField').value;
+            const estCycles = document.getElementById('inputNumber').value;
+            const todo = new Todo(text, estCycles);
+            state.todos.push(todo)
 
-        Todo.render();
+            console.log(state.todos)
+
+            Todo.render();
+        } else if (event.target.id === 'cancelBtn') {
+            Todo.render();
+        }
+
+
     }
 
     static render() {
@@ -50,13 +63,16 @@ export class Todo {
 
         userInt.appendChild(todoDiv)
 
-        //quick input
 
-        const inputField = document.createElement('input');
-        inputField.type = 'text';
-        inputField.id = 'inputField';
-        todoDiv.appendChild(inputField)
-        inputField.addEventListener('keyup', this.addTodoHandler.bind(this))
+        //add todo button
+
+        const addTodoBtn = document.createElement('button');
+        addTodoBtn.type = 'button';
+        addTodoBtn.id = 'addTodoBtn'
+        addTodoBtn.innerText = 'Add Task'
+        console.log(addTodoBtn)
+        userInt.appendChild(addTodoBtn)
+        addTodoBtn.addEventListener('click', this.addTodoScreenCall.bind(this))
 
         //todos
 
@@ -84,10 +100,63 @@ export class Todo {
 
         console.log(renderedTodos)
 
-        userInt.appendChild(renderedTodos);
+        userInt.insertBefore(renderedTodos, addTodoBtn);
 
 
 
 
+    }
+
+    static renderAddTodoWindow() {
+        //Add todo screen
+
+        const addTodoDiv = document.createElement('div');
+
+        // input field
+
+        const inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.id = 'inputField';
+        inputField.placeholder = 'what are you working on?'
+        addTodoDiv.appendChild(inputField)
+
+
+        // Number of estimated pomodoros
+        const title = document.createElement('h3');
+        title.textContent = 'Est Pomodoros'
+        addTodoDiv.appendChild(title);
+
+        const estPomodoros = document.createElement('input');
+        estPomodoros.type = 'number';
+        estPomodoros.min = '0';
+        estPomodoros.step = '1';
+        estPomodoros.value = '1';
+        estPomodoros.id = 'inputNumber'
+        addTodoDiv.appendChild(estPomodoros);
+
+        // save and cancel buttons
+
+        const confirmDiv = document.createElement('div');
+        confirmDiv.id = 'confirmationDiv';
+
+        const cancelBtn = document.createElement('button');
+        cancelBtn.type = 'button';
+        cancelBtn.id = 'cancelBtn';
+        cancelBtn.textContent = 'Cancel'
+
+        const saveBtn = document.createElement('button');
+        saveBtn.type = 'button';
+        saveBtn.id = 'saveBtn';
+        saveBtn.textContent = 'Save'
+
+        confirmDiv.appendChild(cancelBtn);
+        confirmDiv.appendChild(saveBtn);
+
+        addTodoDiv.appendChild(confirmDiv);
+
+        const addTodoBtn = document.getElementById('addTodoBtn');
+        addTodoBtn.parentElement.insertBefore(addTodoDiv, addTodoBtn)
+        document.getElementById('todo').removeChild(addTodoBtn);
+        addTodoDiv.addEventListener('click', this.addTodoHandler.bind(this))
     }
 }
